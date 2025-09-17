ec5e86e6c86d53be76b1e4b74a5c94077367c499
ZVyachelsavV <vvvv.zkv@gmail.com>
2025-09-17 19:14:10 +0300

    add modern normalize, cafeinfo tsx, votes.ts

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,30 +1,20 @@
-import { useState } from 'react';
-import viteLogo from '/vite.svg';
-import './App.css';
+// import { useState } from 'react';
+import CafeInfo from '../CafeInfo/CafeInfo';
+import css from './App.module.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+
+  const votes = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className={css.app}>
+      <CafeInfo />
+    </div>
   );
 }
 
